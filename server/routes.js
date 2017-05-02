ef2e7bfae98973ece45247f18e0b65d53fd8e4b1
Brenner Spear <blspear@gmail.com>
2017-05-02 16:59:13 -0700

    Return raw openweathermap data

--- server/routes.js
+++ server/routes.js
@@ -1,8 +1,6 @@
 var router = require('express').Router()
 var weather = require('./weather')
 
-
 router.get('/weather', weather.forecast);
-
 
 module.exports = router
--- server/weather.js
+++ server/weather.js
@@ -1,4 +1,31 @@
+var weather = require('openweathermap')
+
+weather.defaults({
+  APPID: process.env.OPENWEATHERKEY,
+  cnt: 15,
+  units: 'imperial'
+})
+
+
+const getForcaseAysnc = function(config) {
+  return new Promise(function(resolve,reject) {
+    weather.forecast(config, function(err, config) {
+      if(err !== null) return reject(err)
+      resolve(config)
+    })
+  })
+}
+
+var queryParams = {
+  q: 'san_francisco'
+}
+
 exports.forecast = function(req, res) {
-  console.log(req.query)
-  res.send('hello')
+  // console.log(req.query)
+  getForcaseAysnc(queryParams)
+  .then(data => {
+    res.send(data)
+    console.log('data sent')
+  })
+
 }
